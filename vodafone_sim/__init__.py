f4b9141be011cb3dde40566f2bbe05df0bb4efbe
Lukkunt <lukas.kunt@hardwario.com>
2023-10-13 12:31:58 +0200

    + dataframe with  information about communication

--- vodafone_sim/__init__.py
+++ vodafone_sim/__init__.py
@@ -18,15 +18,21 @@ main('C:\Code\sim\Data\SIM_Inventory_Full.csv',
      "C:\Code\sim\Data\Filtered_Full.csv")
 
 
-def get_data(Itemised_data_usage_for_device_File_Path, Filtered_Data_Output_File_Path):
+def get_data(Itemised_data_usage_for_device_File_Path, Filtered_Data_Output_File_Path, Filter_File_Path):
     data = pd.read_csv(Itemised_data_usage_for_device_File_Path)
-    # cuts full data to 4 collumns
+    # cuts full data to 2 collumns
     data = data[["ICCID", "Total_Kbytes"]]
-    # creates a csv file containing all active vodafone simcards with Has_Been_Active.Test : Y or N
-    data = data.groupby('ICCID').sum()
-    print(data.head())
+    # groups all comunication rows for each device
+    # displays the max "Total_Kbytes" value ---- if it is "0.00" it means that this device did not communicate in the time range of selected report
+    data = data.groupby('ICCID').agg("max")
+    # Saves file to csv
     data.to_csv(Filtered_Data_Output_File_Path)
+    # CREATES FILTER; TRUE != "0.00";
+    filt = (data["Total_Kbytes"] != "0.00")
+    # saves filter
+    filt.to_csv(Filter_File_Path)
+    # filters data to contain only != "0.00" values
 
 
 get_data("C:\Code\sim\Data\Itemised_data_usage_for_device_(STCU)_20231001_20231031_2023-10-10T84546390Z.csv",
-         "C:\Code\sim\Data\Filtered_Itemised.csv")
+         "C:\Code\sim\Data\Filtered_Itemised.csv", "C:\Code\sim\Data\Filter.csv")
