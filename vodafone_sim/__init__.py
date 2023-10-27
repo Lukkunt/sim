9e552eb41d5b82046ade4d8ba581479fcbaf4c66
Lukkunt <lukas.kunt@hardwario.com>
2023-10-27 15:09:24 +0200

    sum of com in each month not working

--- vodafone_sim/__init__.py
+++ vodafone_sim/__init__.py
@@ -6,34 +6,10 @@ from loguru import logger
 input_folder_path = 'C:\Code\sim\Data\Source'
 output_folder_path = 'C:\Code\sim\Data\Output'
 
-# Get a list of file paths in the input folder
-file_paths = [os.path.join(input_folder_path, f) for f in os.listdir(
-    input_folder_path) if f.endswith('.csv')]
 
-itemised_data_paths = []
-sim_inventory_paths = []
-for file_path in file_paths:
-    if "Itemised_data_usage_for_device_(STCU)_" in file_path:
-        itemised_data_paths.append(file_path)
-    elif "SIM_Inventory_Full" in file_path:
-        sim_inventory_paths.append(file_path)
-
-logger.debug(itemised_data_paths)
-logger.debug(sim_inventory_paths)
-
-# Specify the file paths
-Itemised_data_Path = "C:\\Code\\sim\\Data\\Source\\Itemised_data_usage_for_device_(STCU)_20231001_20231020_2023-10-21T123413273Z.csv"
-Filtered_Itemised_Path = "C:\\Code\\sim\\Data\\Output\\Filtered_Itemised.csv"
-Filter_Path = "C:\\Code\\sim\\Data\\Output\\Filter.csv"
-Filtered_Full = "C:\\Code\\sim\\Data\\Output\\Filtered_Full.csv"
-Output_Path = "C:\\Code\\sim\\Data\\Output\\Output.csv"
-input_folder_path = 'C:\Code\sim\Data\Source'
-output_folder_path = 'C:\Code\sim\Data\Output'
-
-
-def process_sim_data(Itemised_data_Path, Filtered_Itemised_Path, Filter_Path):
+def process_sim_data(source_file_path, filtered_file_path, filter_Path):
     # Read data from the CSV file
-    data = pd.read_csv(Itemised_data_Path)
+    data = pd.read_csv(source_file_path)
 
     # Keep only the "ICCID" and "Total_Kbytes" columns
     data = data[["ICCID", "Total_Kbytes"]]
@@ -65,7 +41,7 @@ def process_sim_data(Itemised_data_Path, Filtered_Itemised_Path, Filter_Path):
     total_sum = data['Total_Kbytes'].sum()
 
     # Save the filtered data to a CSV file
-    data.to_csv(Filtered_Itemised_Path)
+    data.to_csv(filtered_file_path)
 
     # Create a filter for values not equal to 0.00
     filt = (data["Total_Kbytes"] != 0.00)
@@ -74,7 +50,7 @@ def process_sim_data(Itemised_data_Path, Filtered_Itemised_Path, Filter_Path):
     filt = filt.rename("Active")
 
     # Save the filter to a CSV file (you may want to save it as a boolean mask)
-    filt.to_csv(Filter_Path)
+    filt.to_csv(filter_Path)
 
     # Filter data to contain only non-zero values
     non_zero_data = data[filt]
@@ -86,14 +62,45 @@ def process_sim_data(Itemised_data_Path, Filtered_Itemised_Path, Filter_Path):
     return total_sum, non_zero_data
 
 
-for path in itemised_data_paths:
-    logger.info(path)
-    total_sum, non_zero_data = process_sim_data(
-        path, Filtered_Itemised_Path, Filter_Path)
-    print(f"Total sum for {path}: {total_sum}")
+def join_full_filter(filtered_file_path, filtered_full_path):
 
- # Loop through all files in the folder
+    df1 = pd.read_csv(filtered_file_path)
+
+    df2 = pd.read_csv(filtered_full_path)
+
+    df1 = df1.set_index("ICCID")
+    df1 = df1.rename(columns={"Total_Kbytes": info})
+    df2 = df2.set_index("ICCID")
+
+    df3 = df2.join(df1)
+    df3.to_csv(filtered_full_path)
+
+
+def sim_inventory_filter(input_folder_path, output_folder_path):
+
+    for filename in os.listdir(input_folder_path):
+
+        if filename.startswith('SIM_Inventory_Full___'):
+            logger.debug("1")
+            info = filename.split('SIM_Inventory_Full___')[
+                1].split('.')[0]
+            # Construct the full path to the file
+            source_file_path = os.path.join(input_folder_path, filename)
+            # Construct the output file paths
+            filtered_full_path = os.path.join(
+                output_folder_path, 'filtered_SIM_Inventory_Full___' + info + '.csv')
+            df1 = pd.read_csv(source_file_path)
+            df1 = df1[["ICCID", "IMSI", "SIM_State"]]
+            df1.to_csv(filtered_full_path)
+            return filtered_full_path
+
+
+filtered_full_path = sim_inventory_filter(
+    input_folder_path, output_folder_path)
+
+# Loop through all files in the folder
 for filename in os.listdir(input_folder_path):
+
     # Check if the filename starts with "Itemised_data_usage_for_device_(STCU)_"
     if filename.startswith('Itemised_data_usage_for_device_(STCU)_'):
         # Extract the information after "Itemised_data_usage_for_device_(STCU)_"
@@ -101,7 +108,7 @@ for filename in os.listdir(input_folder_path):
             1].split('.')[0]
 
         # Construct the full path to the file
-        file_path = os.path.join(input_folder_path, filename)
+        source_file_path = os.path.join(input_folder_path, filename)
 
         # Construct the output file paths
         filtered_file_path = os.path.join(
@@ -111,24 +118,20 @@ for filename in os.listdir(input_folder_path):
 
         # Call the process_sim_data() function for the current file
         total_sum, non_zero_data = process_sim_data(
-            file_path, filtered_file_path, filter_path) 
+            source_file_path, filtered_file_path, filter_path)
 
-# Print the total sum of Total_Kbytes
-
-
-def join_full_filter(Filtered_Full, Filter, Filtered_Itemised_Path, Output_Path):
-    df1 = pd.read_csv(Filtered_Full)
-    df2 = pd.read_csv(Filter_Path)
-    df3 = pd.read_csv(Filtered_Itemised_Path)
-
-    df1 = df1.set_index("ICCID")
-    df2 = df2.set_index("ICCID")
-    df3 = df3.set_index("ICCID")
-
-    df4 = df1.join([df2, df3])
-
-    df4.to_csv(Output_Path)
+        join_full_filter(filtered_file_path,
+                         filtered_full_path)
 
 
-join_full_filter(Filtered_Full, Filter_Path,
-                 Filtered_Itemised_Path, Output_Path)
+def sum_all_comunication(filtered_full_path):
+    df1 = pd.read_csv(filtered_full_path)
+    df1 = df1.drop(['Unnamed: 0', 'IMSI', 'SIM_State'], axis=1)
+    df1.set_index("ICCID")
+    column_names = df1.columns.tolist()
+    print(df1[column_names].sum().head())
+
+    return df1
+
+
+sum_all_comunication(filtered_full_path)
